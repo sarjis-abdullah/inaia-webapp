218a6174f4e463c3535c7ca25e08584608697fae
Mahdi Njim <mahdinjim@Mahdis-MacBook-Pro.local>
2023-07-19 10:44:42 +0100

    merge

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/Users/mahdinjim/Documents/workspace/inaia/client/inaia-webapp/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default defuFn(inlineConfig)
