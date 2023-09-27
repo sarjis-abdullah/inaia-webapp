d2001af7136299eb23d7a25a92a29c51716132f3
Mahdi Njim <mahdinjim@Mahdis-MacBook-Pro.local>
2023-09-27 10:32:46 +0100

    order countries by code

--- lib/services/CountryService.ts
+++ lib/services/CountryService.ts
@@ -6,8 +6,8 @@ export class CountryService{
     private static links = Urls.URLS();
     private static requester = HttpRequester.httpRequester();
     private static headers = new HttpHeader();
-    public static async getCountryList(all:Boolean):Promise<Array<Country>>{
-        let res:Array<Country> = await this.requester.get(this.links.getCountries(),this.headers);
+    public static async getCountryList(all:Boolean,isCode=false):Promise<Array<Country>>{
+        let res:Array<Country> = await this.requester.get(this.links.getCountries(isCode),this.headers);
         if(all)
             return res;
         else
