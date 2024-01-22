9ea67e0cdc949b2068a57ffd627bfb075a402d57
Sarjis Abdullah <55045212+sarjis-abdullah@users.noreply.github.com>
2024-01-22 14:26:12 +0600

    extra: dot nuxt code updated (by itself)

--- .nuxt/types/middleware.d.ts
+++ .nuxt/types/middleware.d.ts
@@ -1,6 +1,6 @@
 import type { NavigationGuard } from 'vue-router'
 export type MiddlewareKey = "protected"
-declare module "/Users/mahdinjim/Documents/workspace/inaia/client/inaia-webapp/node_modules/nuxt/dist/pages/runtime/composables" {
+declare module "/home/sarjis/Inaia/inaia-webapp/node_modules/nuxt/dist/pages/runtime/composables" {
   interface PageMeta {
     middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
   }
