c6f03d5d01c3134b7da658bf81d25b096ca42de9
Sarjis Abdullah <55045212+sarjis-abdullah@users.noreply.github.com>
2024-02-08 16:07:44 +0600

    feat: status component added

---
+++ components/SupportTicket/SupportTicketStatus.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="bg-green-500" :class="statusClass" v-if="status == 'new'">
+        <slot></slot>
+    </div>
+    <div class="bg-blue-500" :class="statusClass" v-else-if="status == 'closed'">
+        <slot></slot>
+    </div>
+    <div class="bg-blue-500" :class="statusClass" v-else-if="status == 'open' || status == 'has_new_answer'">
+        <slot></slot>
+    </div>
+    <div class="bg-red-500" :class="statusClass" v-else-if="status == 'has_new_message'">
+        <slot></slot>
+    </div>
+    <div class="default" :class="statusClass" v-else>
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        status: {
+            class: String
+        }
+    },
+    data() {
+        return {
+            statusClass: "text-white py-1 px-2 rounded-md text-xs"
+        }
+    },
+}
+</script>
