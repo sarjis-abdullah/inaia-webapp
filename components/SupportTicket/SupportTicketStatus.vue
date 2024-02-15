240dc6a43831c58a13afe51adef0902373558997
Sarjis Abdullah <55045212+sarjis-abdullah@users.noreply.github.com>
2024-02-15 17:02:06 +0600

    refactor: include and status commonly used

--- components/SupportTicket/SupportTicketStatus.vue
+++ components/SupportTicket/SupportTicketStatus.vue
@@ -1,31 +1,28 @@
 <template>
-    <div class="bg-[#1aae6f]" :class="statusClass" v-if="status == 'new'">
+    <div class="bg-[#1aae6f]" :class="statusClass" v-if="status == SupportTicketStatusList.new">
         <slot></slot>
     </div>
-    <div class="bg-red-500" :class="statusClass" v-else-if="status == 'closed'">
+    <div class="bg-red-500" :class="statusClass" v-else-if="status == SupportTicketStatusList.closed">
         <slot></slot>
     </div>
-    <div class="bg-blue-500" :class="statusClass" v-else-if="status == 'open' || status == 'has_new_answer'">
+    <div class="bg-blue-500" :class="statusClass" v-else-if="status == SupportTicketStatusList.open || status == SupportTicketStatusList.has_new_answer">
         <slot></slot>
     </div>
-    <div class="bg-[#1aae6f]" :class="statusClass" v-else-if="status == 'has_new_message'">
+    <div class="bg-[#1aae6f]" :class="statusClass" v-else-if="status == SupportTicketStatusList.has_new_message">
         <slot></slot>
     </div>
     <div class="default" :class="statusClass" v-else>
         <slot></slot>
     </div>
 </template>
-<script>
-export default {
-    props: {
-        status: {
-            class: String
-        }
-    },
-    data() {
-        return {
-            statusClass: "text-white py-1 px-2 rounded-md text-xs"
-        }
-    },
-}
+<script setup lang="ts">
+import {ref, PropType} from 'vue'
+import { SupportTicketStatusList } from '@/lib/contants/SupportTicketStatusList'
+defineProps({
+    status:{
+        type: String as PropType<SupportTicketStatusList>,
+        required: true
+    }
+})
+const statusClass = "text-white py-1 px-2 rounded-md text-xs"
 </script>
