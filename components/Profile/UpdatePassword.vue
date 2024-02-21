eb08c77e32a4d1a64309fc19494b55f379a4d5a4
Sarjis Abdullah <55045212+sarjis-abdullah@users.noreply.github.com>
2024-02-21 15:08:10 +0600

    refactor: old code re-used

--- components/Profile/UpdatePassword.vue
+++ components/Profile/UpdatePassword.vue
@@ -9,7 +9,7 @@
                         {{ $t('old_password') }}
                     </label>
                     <div class="relative mt-1 rounded-md shadow-sm">
-                        <input type="text" name="old_password" id="old_password" v-model="state.old_password"
+                        <input :type="inputType" name="old_password" id="old_password" v-model="state.old_password"
                             class="block  w-full 10 pl-3 py-2 rounded-md"
                             :class="!state.old_password || !showOldPasswordError ? inputStyle : inputErrorStyle" />
                     </div>
@@ -19,7 +19,7 @@
                         {{ $t('new_password') }}
                     </label>
                     <div class="relative mt-1 rounded-md shadow-sm">
-                        <input type="text" name="password" id="password" v-model="state.password"
+                        <input :type="inputType" name="password" id="password" v-model="state.password"
                             class="block  w-full 10 pl-3 py-2 rounded-md"
                             :class="!state.password || !showNewPasswordError ? inputStyle : inputErrorStyle" />
                     </div>
@@ -27,26 +27,24 @@
                 <div>
                     <label for="password_confirmation" class="block text-sm font-medium text-gray-700">
                         {{ $t('password_confirmation') }}</label>
-                    <div class="relative mt-1 rounded-md shadow-sm">
-
-                        <input type="text" name="password_confirmation" id="password_confirmation" v-model="state.password_confirmation"
-                            class="block  w-full 10 pl-3 py-2 rounded-md"
-                            :class="!state.password_confirmation || !showConfirmPasswordError ? inputStyle : inputErrorStyle" />
+                    <div class="mt-1">
+                        <input id="password" name="password" :type="inputType" v-model="state.password_confirmation"
+                            :class="confirmedPasswordValidated ? 'block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6' : inputErrorStyle" />
                     </div>
+                    <p class="mt-1 text-center text-sm text-red-500" v-if="!confirmedPasswordValidated">{{
+                        $t('confirm_password_is_required') }}</p>
                 </div>
 
                 <p class="mt-2 text-sm text-red-600" id="email-error" v-if="submittingError">{{ $t('account_info_error') }}
                 </p>
-                <p class="mt-2 text-sm text-red-600" id="email-error" v-if="state.password && state.password_confirmation && !passwordMatched">{{ $t('password_mismatch_alert') }}
-                </p>
-                <p class="mt-2 text-sm text-yellow-600">{{ $t('password_update_alert') }}
+                <p class="mt-2 text-sm text-yellow-600">{{ $t('password_mismatch_alert') }}
                 </p>
                 <div class="mt-8 text-center">
-                    <button v-if="!isSubmitting" type="submit" :disabled="disableSubmition || isSubmitting" @click.prevent="save"
-                        :class="(disableSubmition || isSubmitting) ? 'opacity-50' : 'opacity-100'"
+                    <button v-if="!isSubmitting" type="submit" :disabled="disabled || isSubmitting" @click.prevent="save"
+                        :class="(disabled || isSubmitting) ? 'opacity-50' : 'opacity-100'"
                         class="flex w-full justify-center rounded-md border border-transparent bg-blue-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">{{
                             $t('save') }}</button>
-                    <Loading v-else/>
+                    <Loading v-else />
                     <InLineApiError :err="submitErr" />
                 </div>
             </form>
@@ -54,7 +52,7 @@
     </div>
 </template>
 <script lang="ts" setup>
-import { ExclamationCircleIcon } from '@heroicons/vue/20/solid';
+import { validatePassword } from '@/lib/utils/Validators';
 import LogoutHelper from '@/helpers/LogoutHelper';
 const router = useRouter();
 import { ref, reactive, computed } from 'vue';
@@ -65,12 +63,14 @@ import InLineApiError from '@/components/common/InLineApiError.vue';
 const isSubmitting = ref(false);
 const inputErrorStyle = 'border-red-300   text-red-900 placeholder-red-300 focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm';
 const inputStyle = 'border-gray-300 focus:border-blue-500 focus:ring-blue-500 sm:text-sm';
-const minimumPasswordLength = 8
-
 const { t } = useI18n();
 const submittingError = ref(false);
+const passwordValidated = ref(true);
+const emailValidated = ref(true);
+const confirmedPasswordValidated = ref(true)
+const disabled = ref(true);
 const submitErr = ref(null);
-const inputType = ref("text");
+const inputType = ref("password");
 const state = reactive({
     old_password: '',
     password: '',
@@ -81,46 +81,13 @@ const emit = defineEmits<{
 }>()
 
 //computed
-const showOldPasswordError = computed(()=> {
-    
-    return !((state.old_password.length > 0) && (state.old_password.length >= minimumPasswordLength))
-})
-const showNewPasswordError = computed(()=> {
-    
-    return !((state.password.length > 0) && (state.password.length >= minimumPasswordLength))
-})
-const showConfirmPasswordError = computed(()=> {
-    
-    return !((state.password_confirmation.length > 0) && (state.password_confirmation.length >= minimumPasswordLength))
-})
+const showOldPasswordError = computed(() => {
 
-const shouldShowRequiredError = computed(() => {
-    const { old_password, password, password_confirmation } = state
-    if (!old_password || !password || !password_confirmation) {
-        return true
-    }
-    const c1 = showOldPasswordError.value
-    const c2 = showNewPasswordError.value
-    const c3 = showConfirmPasswordError.value
-    if (c1 || c2 || c3) {
-        return true
-    }
-    return false
+    return !((state.old_password.length > 0))
 })
-const passwordMatched = computed(()=> {
-    if (state?.password === state?.password_confirmation) {
-        return true
-    }
-    return false
-})
-const disableSubmition = computed(() => {
-    if (shouldShowRequiredError.value) {
-        return true
-    }
-    if (state.password === state.password_confirmation) {
-        return false
-    }
-    return true
+const showNewPasswordError = computed(() => {
+
+    return !((state.password.length > 0))
 })
 
 async function save() {
@@ -139,5 +106,13 @@ async function save() {
         isSubmitting.value = false;
     }
 }
+watch(state, (currentValue) => {
+    if (state.password && state.password_confirmation) {
+        passwordValidated.value = validatePassword(currentValue.password);
+        confirmedPasswordValidated.value = passwordValidated.value && (currentValue.password_confirmation == currentValue.password)
+        disabled.value = !(passwordValidated.value && emailValidated.value && confirmedPasswordValidated.value)
+    }
+
+})
 
 </script>
