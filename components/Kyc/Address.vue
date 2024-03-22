5f16ec3209e707c8f231d1d89ce49f565fd8da0b
Mahdi Njim <mahdinjim@Mahdis-MacBook-Pro.local>
2024-03-22 11:54:54 +0100

    program gotodashboard action

--- components/Kyc/Address.vue
+++ components/Kyc/Address.vue
@@ -81,11 +81,11 @@
         </div>
         <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10" v-else-if="isVerified">
             <p class="text-center my-6">{{ $t('accountIsAlreadyVerified') }}</p>
-            <a class="flex w-full justify-center rounded-md border border-transparent bg-blue-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">{{ $t('goToDashboard') }}</a>
+            <a class="flex w-full justify-center rounded-md border border-transparent bg-blue-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2" @click="goToDashboard">{{ $t('goToDashboard') }}</a>
         </div>
         <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10" v-else-if="isPending">
             <p class="text-center my-6">{{ $t('kycStatusIsPending') }}</p>
-            <a class="flex w-full justify-center rounded-md border border-transparent bg-blue-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">{{ $t('goToDashboard') }}</a>
+            <a class="flex w-full justify-center rounded-md border border-transparent bg-blue-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2" @click="goToDashboard">{{ $t('goToDashboard') }}</a>
         </div>
     </div>
 </template>
