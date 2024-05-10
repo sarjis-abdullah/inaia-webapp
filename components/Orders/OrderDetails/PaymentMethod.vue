b57bfc830b6270da8fd26794ae1d553898ff1b58
Sarjis Abdullah <55045212+sarjis-abdullah@users.noreply.github.com>
2024-05-10 22:04:45 +0600

    added payment details

--- components/Orders/OrderDetails/PaymentMethod.vue
+++ components/Orders/OrderDetails/PaymentMethod.vue
@@ -1,13 +1,37 @@
 <template>
     <div>
-        <ListItem :title="$t('payment_method')" v-if="paymentMethodName"><span>{{$t(paymentMethodName)}}</span></ListItem>
+        <div class="px-0 py-2 flex justify-between">
+            <div class="px-0 justify-center font-semibold">{{$t('payment_method')}}</div>
+            <div class="px-0 text-sm flex items-center gap-1">
+                <span>{{$t(paymentMethodName)}}</span>
+                <span v-if="order?.payment_details">
+                    <ChevronDownIcon v-if="!showPaymentDetails" @click="toggle" class="h-3 w-3 cursor-pointer"/>
+                    <ChevronUpIcon v-else @click="toggle" class="h-3 w-3 cursor-pointer"/>
+                </span>
+            </div>
+        </div>
+        <div v-if="showPaymentDetails && order?.payment_details" class="ml-2">
+            <ListItem :title="$t('iban')" v-if="paymentMethodName">
+                <span class="pl-2">
+                    {{ order.payment_details.inaia_iban }}
+                </span>
+            </ListItem>
+            <ListItem :title="$t('reference')" v-if="paymentMethodName">
+                <span>
+                    {{ order.payment_details.reference }}
+                </span>
+            </ListItem>
+        </div>
     </div>
 </template>
 <script lang="ts" setup>
 import ListItem from '@/components/common/ListItem';
-import {computed} from 'vue';
+import { ChevronDownIcon } from '@heroicons/vue/24/outline';
+import { ChevronUpIcon } from '@heroicons/vue/24/outline';
+import {computed, ref} from 'vue';
 import { type Order } from '@/lib/models';
 import { OrderService } from '@/lib/services';
+const showPaymentDetails = ref(false)
 const props = defineProps({
     order:{
         type: Object as PropType<Order>
@@ -16,4 +40,8 @@ const props = defineProps({
 const paymentMethodName = computed(()=>{
     return OrderService.getPaymentMethodName(props.order);
 })
+
+const toggle = () => {
+    showPaymentDetails.value = !showPaymentDetails.value
+}
 </script>
