ffe7b0b79e7dcae52394ad05b81673a383ea938a
Sarjis Abdullah <55045212+sarjis-abdullah@users.noreply.github.com>
2024-07-28 23:38:56 +0600

    const updated

--- lib/contants/Constants.ts
+++ lib/contants/Constants.ts
@@ -6,7 +6,4 @@ export const CODE_INPUT_LENGTH_FOUR = 4;
 export const CODE_INPUT_LENGTH_SIX = 6;
 export const MINIMUM_PERIOD_TO_START_SAVING_PLAN = 14;
 export const WAIT_PERIOD = 7;
-export const TWO_FA_METHOD = "2fa";
-export const CODE_INPUT_LENGTH_SIX = 6;
-export const CODE_INPUT_LENGTH_FOUR = 4;
 export const MFA_SECRET_TRANSLATION_KEY = "mfaSecret";
